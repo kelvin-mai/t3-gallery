6a86d7cba5f45838e5ecebe046fa5b31fee91837
Kelvin Mai <kelvin.mai002@gmail.com>
2024-04-15 00:37:11 -0700

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+const mockUrls = [
+  "https://utfs.io/f/dfe57453-0c7a-42b5-a080-f8b141b19427-mletr9.png",
+  "https://utfs.io/f/02829c9b-5b0b-454b-95a1-50d5e9c4f6f2-ke09p4.png",
+  "https://utfs.io/f/be9831d9-1d4b-43c8-a5c2-a2b806d301e4-asiuhe.png",
+  "https://utfs.io/f/02feaae1-6e77-4dfc-8176-72c6ee2c14d5-mletra.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((img) => (
+          <div key={img.id} className="w-48">
+            <img src={img.url} alt="image" />
+          </div>
+        ))}
+        Hello (gallery in progress)
+      </div>
     </main>
   );
 }
