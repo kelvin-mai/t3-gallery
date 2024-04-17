9336755712dc5764f1354a0a67dba54c132a8618
Kelvin Mai <kelvin.mai002@gmail.com>
2024-04-16 17:18:57 -0700

    added file uploads with uploadthing

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -2,16 +2,25 @@
 
 import { SignedIn, UserButton } from "@clerk/clerk-react";
 import { SignInButton, SignedOut } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((img) => (
+        <div key={img.id} className="w-48">
+          <img src={img.url} alt={img.name} />
+          <div>{img.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((img) => (
-          <div key={img.id} className="w-48">
-            <img src={img.url} alt={img.name} />
-            <div>{img.name}</div>
-          </div>
-        ))}
-        Hello (gallery in progress)
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
