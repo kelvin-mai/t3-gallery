c30d154d5b387cfa3e686726b04a01b4ce5be01a
Kelvin Mai <kelvin.mai002@gmail.com>
2024-04-21 21:58:29 -0700

    added simple upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -2,25 +2,18 @@
 
 import { SignedIn, UserButton } from "@clerk/clerk-react";
 import { SignInButton, SignedOut } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
