a99cab446623a74c2381c2ac9c34fd74ce77f16b
Kelvin Mai <kelvin.mai002@gmail.com>
2024-04-21 21:14:26 -0700

    finalized modal and prallell image routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,6 +1,25 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView({ id }: { id: number }) {
   const image = await getImage(id);
-  return <img src={image.url} className="w-96" />;
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-lg">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
